ec5c8a1ecfdc273b6b539ebbb1794ea5ceeba253
Alireza Sahabi <alireza.sahabi2002@gmail.com>
2024-12-10 21:28:30 +0330

    file-based routing & react server components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,19 @@
+/**
+ * "page" file name tells NextJS that it should render a page.
+ */
+
+/**
+ * It's regular React component but treated in a special way by NextJS.
+ * It's treated as a server component & executed on the server.
+ */
+
 export default function Home() {
+  /**
+   * We can see this on the back-end, in the terminal that we're running the app next to other server logs.
+   * Or in the console like this: Server Hoy!
+   * console.log("Hoy!");
+   */
+
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
