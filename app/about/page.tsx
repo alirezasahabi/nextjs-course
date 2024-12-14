add76dca9bb92fb6b1a1fdae0efbf9312e3e6b58
Alireza Sahabi <alireza.sahabi2002@gmail.com>
2024-12-14 19:28:43 +0330

    create a new route

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+/**
+ * In the "app" directory we can create new paths, which we wanna handle as routes
+ * by adding new folders & in those folders add the "page" file which returns a React component.
+ * The name of the component doesn't matter.
+ */
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>AboutPage</h1>
+    </main>
+  ) ;
+};
+
+export default AboutPage;
