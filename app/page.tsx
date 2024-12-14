972696b4016705ddc4e3a51776698c49da4dcd71
Alireza Sahabi <alireza.sahabi2002@gmail.com>
2024-12-14 22:29:39 +0330

    custom component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
  */
 
 import Link from "next/link";
+import Header from "@/components/header";
 
 /**
  * It's regular React component but treated in a special way by NextJS.
@@ -18,8 +19,7 @@ export default function Home() {
 
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
+      <Header />
       <p>🔥 Let&apos;s get started! 🔥</p>
       <p>
         {/* <a href="/about">About</a> */}
