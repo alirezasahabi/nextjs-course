c6e032e0ddc8771674679013a9e87bf713d04491
Alireza Sahabi <alireza.sahabi2002@gmail.com>
2025-01-11 21:01:05 +0330

    foodies: trigger cache revalidation

--- foodies/lib/actions.ts
+++ foodies/lib/actions.ts
@@ -9,6 +9,7 @@
 
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
+import { revalidatePath } from "next/cache";
 
 const isInvalidText = (text: any) => !text || text.trim() === "";
 
@@ -46,9 +47,31 @@ export async function shareMeal(
      * Which means, for example it shouldn't include any methods,
      * because those would get lost whilst beind send to client.
      */
-    return { message: "Invalid input!"  };
+    return { message: "Invalid input!" };
   }
 
   await saveMeal(meal as any);
+  /**
+   * If we build our app production & then run it, when we create a new data(EX: add meal in this project),
+   * we can't see that in the app. Also if we reload the "/meals" it reloads so fast
+   * although we add a delay in fetching meals.
+   * The reason is that NextJS performs aggresive caching with an extra step when building production.
+   * NextJS pre-renders all the pages of the app that can be pre-generated.
+   * So all data will be fetched during the build process.
+   * By pre-rending these pages, it has those pages availabe tight from the start after being deployed.
+   * The downside of this approach is that, it necer refetches data.
+   * 
+   * To fiX this, we need to tell NextJS to throw away its cache or parts of it whenever we create new data.
+   * "revalidatePath" built-in function provided by NextJS which tells it to revalidate the cache that
+   * belongs to a certain route path.
+   * This fucntion also accepts another parameter which can be "page"(default) or "layout".
+   * By default only this path will be revalidated & no nested paths.
+   * If we set it to "layout", it's the layout that will be revalidated & therefore
+   * all nested pages will be revalidated.
+   * 
+   * Revalidate all the pages of the website.
+   * revalidatePath("/", "layout");
+   */ 
+  revalidatePath("/meals");
   redirect("/meals");
 }
