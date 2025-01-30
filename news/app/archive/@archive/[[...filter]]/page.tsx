7d21a686d35119b3bf95c84bf62629951ef8c224
Alireza Sahabi <alireza.sahabi2002@gmail.com>
2025-01-30 15:46:18 +0330

    news: update the content based on the url slug

--- news/app/archive/@archive/[[...filter]]/page.tsx
+++ news/app/archive/@archive/[[...filter]]/page.tsx
@@ -13,46 +13,73 @@
  * So if we remove all parameters & go to '/archive', we get 404.
  * To fix this & make the slug paramater optional,
  * we have to wrap it in double square brackets: [[...slug]].
- * 
+ *
  * NOTE: With the current implementation we'll get an error:
  *       You cannot define a route with the same specificity as a optional catch-all route ("/archive" and "/archive[[...filter]]").
  *       This means that we have t2o conflicting "page" files.
  *       "[[...filter]]" will catch all the segments after "/archive".
  *       But we have two "page" file: archive/page _ archive/[[...filter]]/page
  *       To fix this we should remove the "page" file in the archive folder.
- *       
+ *
  */
 
 import Link from "next/link";
-import { getAvailableNewsYears } from "@/lib/news";
+import NewsList from "@/components/news-list";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
+import { News } from "@/dummy-news";
 
 interface Props {
   params: Promise<{ filter?: string[] }>;
 }
 const FilteredNewsPage = async ({ params }: Props) => {
-  const years = getAvailableNewsYears();
-
   const filter = (await params).filter;
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
 
-  console.log(filter);
+  let links = getAvailableNewsYears();
+  let newsList: News[] = [];
+
+  if (selectedYear && !selectedMonth) {
+    links = getAvailableNewsMonths(selectedYear);
+    newsList = getNewsForYear(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    links = [];
+    newsList = getNewsForYearAndMonth(selectedYear, selectedMonth);
+  }
+
+  let newsContent = <p>Not news found for the selected period!</p>;
+
+  if (newsList.length > 0) newsContent = <NewsList list={newsList} />;
 
   return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {years.map((year) => (
-            <li key={year}>
-              <Link href={`/archive/${year}`}>{year}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
   );
-
-  // const filteredNews = getNewsForYear(year);
-
-  // return <NewsList list={filteredNews} />;
 };
 
 export default FilteredNewsPage;
